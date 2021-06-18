2bc025b000551095d96d76872f87149425054495
pabloBolivar <83661463+pabloBolivar@users.noreply.github.com>
2021-06-18 14:13:32 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
 import CharacterManager from "./components/CharacterManager";
 
 const rootElement = document.getElementById("app");
